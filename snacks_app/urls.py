a1d9cfa5fd0b51869a439eabc286ee147023930d
Farah Riad Eilouti <fara7.eilouti@gmail.com>
2022-12-03 22:32:04 +0200

    continue working on new lab

--- snacks_app/urls.py
+++ snacks_app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import SnackListView
+from .views import SnackListView,SnackDetail
 urlpatterns=[
-    path('',SnackListView.as_view(), name='Snacks')
+    path('',SnackListView.as_view(), name='Snacks'),
+    path('<int:pk>',SnackDetail.as_view(), name='Detail')
+
 ]
